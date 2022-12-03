4294ea68465f6c3061992b17eee028db2c669da2
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2022-12-03 10:42:38 -0300

    Day 3

--- 2022/Day 3/1 & 2.py
+++ 2022/Day 3/1 & 2.py
@@ -6,5 +6,26 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 with open(os.path.join(__location__, 'input.txt')) as f:
     allLines = f.readlines()
 
+count = 0
+elvesGroup = []
+sum = 0
+
+def PriorityDetermination():
+    global sum, count, elvesGroup
+
+    itemType = elvesGroup[0].intersection(elvesGroup[1]).intersection(elvesGroup[2]).pop()
+    priority = ord(itemType) - (96 if itemType.islower() else 38)
+    print(itemType, priority)
+    sum += priority
+
+    count = 0
+    elvesGroup = []
+
 for line in allLines:
-    pass
+    count += 1
+
+    elvesGroup.append(set(line.strip()))    
+    if count >= 3:
+        PriorityDetermination()
+
+print(sum)
