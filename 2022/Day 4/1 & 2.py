6908bbac4b744deeb8b3f2f9e22f8098758a95e0
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2022-12-04 12:34:50 -0300

    Day 4

--- 2022/Day 4/1 & 2.py
+++ 2022/Day 4/1 & 2.py
@@ -6,5 +6,16 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 with open(os.path.join(__location__, 'input.txt')) as f:
     allLines = f.readlines()
 
+sum = 0
 for line in allLines:
-    pass
+    elves = line.strip().split(',')
+
+    a1, a2 = elves[0].split('-')
+    b1, b2 = elves[1].split('-')
+    a1, a2, b1, b2 = int(a1), int(a2) + 1, int(b1), int(b2) + 1
+    A, B = set(range(b1, b2)), set(range(a1, a2))
+
+    sum += len(A & B) > 0
+    print(sum)
+
+print(sum) 
