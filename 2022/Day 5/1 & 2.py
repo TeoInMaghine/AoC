8a24c9649a8c020c11d2bca3a7c77b028cde8a22
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2022-12-05 13:10:30 -0300

    Day 5

--- 2022/Day 5/1 & 2.py
+++ 2022/Day 5/1 & 2.py
@@ -1,10 +1,38 @@
 import os
-# import heapq
 
 allLines = []
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 with open(os.path.join(__location__, 'input.txt')) as f:
     allLines = f.readlines()
 
+instructions = False
+
+stacks = [[] for i in range(9)]
+
 for line in allLines:
-    pass
+    if line == "\n":
+        instructions = True
+        continue
+
+    if not instructions:
+        for i in range(int(len(line) / 4)):
+            crate = line[i * 4 + 1]
+            if crate.isspace() or crate.isdigit(): continue
+            stacks[i].append(crate)
+    else:
+        processedLine = line[5:].replace("from ", "").replace("to ", "")
+        crateCount, fromStack, toStack = map(int, processedLine.split())
+        fromStack -= 1
+        toStack -= 1
+
+        newFromStack = stacks[fromStack][crateCount:]
+        movedCrates = stacks[fromStack][:crateCount]
+        # movedCrates.reverse() # literally the only difference between 1 & 2
+
+        stacks[toStack][0:0] = movedCrates
+        stacks[fromStack] = newFromStack
+
+answ = ""
+for stack in stacks:
+    answ += stack[0]
+print(answ)
