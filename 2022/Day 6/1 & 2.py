dc16ba5c45e5a3ea8e90198bbe2bd5bc45165a18
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2022-12-06 18:09:30 -0300

    Day 6

--- 2022/Day 6/1 & 2.py
+++ 2022/Day 6/1 & 2.py
@@ -5,5 +5,21 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 with open(os.path.join(__location__, 'input.txt')) as f:
     allLines = f.readlines()
 
-for line in allLines:
-    pass
+def ElementsAreDifferent(list):
+    return len(list) == len(set(list))
+
+def Solve():
+    marker = []
+    i = 0
+    for character in allLines[0]:
+        marker.append(character)
+        i += 1
+
+        if len(marker) == 15:
+            marker.pop(0)
+            if ElementsAreDifferent(marker):
+                return i
+    
+        # print(marker)
+
+print(Solve())
