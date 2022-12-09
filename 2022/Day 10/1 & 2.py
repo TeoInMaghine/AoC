c4a5ee4b0d6721f3e8ec65b03b5569bf9eed6a08
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2022-12-09 15:55:10 -0300

    Day 9

---
+++ 2022/Day 10/1 & 2.py
@@ -0,0 +1,9 @@
+import os
+
+allLines = []
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+with open(os.path.join(__location__, 'input.txt')) as f:
+    allLines = f.readlines()
+
+for line in allLines:
+    pass
