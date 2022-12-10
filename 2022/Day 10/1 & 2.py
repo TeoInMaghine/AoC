6541d66b2710772e3d0dbe8a2f6630cd85cb310f
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2022-12-10 11:16:35 -0300

    Day 10

--- 2022/Day 10/1 & 2.py
+++ 2022/Day 10/1 & 2.py
@@ -5,5 +5,23 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 with open(os.path.join(__location__, 'input.txt')) as f:
     allLines = f.readlines()
 
+answ = ""
+X = 1
+cycle = 1
+def GetCRTPixel():
+    pixel = '#' if (X - 1 <= (cycle % 40 - 1) <= X + 1) else '.'
+    if cycle % 40 == 0: pixel += '\n'
+    return pixel
+
 for line in allLines:
-    pass
+    answ += GetCRTPixel()
+    if line.startswith("noop"):
+        cycle += 1
+    else:
+        number = int(line.split()[1])
+        cycle += 1        
+        answ += GetCRTPixel()
+        cycle += 1        
+        X += number
+    
+print(answ)
