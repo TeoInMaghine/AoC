e359f0bdba6724a3d20d4db06e5092119735a6f8
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2023-12-12 20:40:42 -0300

    Day 1 finished

--- 2023/Day 1/1.py
+++ 2023/Day 1/1.py
@@ -5,9 +5,26 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 with open(os.path.join(__location__, 'input.txt')) as f:
     allLines = f.readlines()
 
+string_numbers = {"zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
+result = 0
 for line in allLines:
-    if line == '\n':
-        print("End")
-    else:
-        print(int(line.strip()))
 
+    sLine = line.strip()
+    nums = []
+    string = ""
+
+    for char in sLine:
+        if char.isnumeric():
+            nums.append(int(char))
+            string = ""
+        else:
+            string += char
+            if len(string) >= 3:
+                for key in string_numbers.keys():
+                    if string.endswith(key):
+                        nums.append(string_numbers[key])
+
+    print(nums)
+    result += 10 * nums[0] + nums[-1]
+
+print(result)
