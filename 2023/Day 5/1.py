c95027b60407114432f96ee7892fbc38198bf044
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2023-12-16 20:57:03 -0300

    Fuck that didn't work either
    I'll try ranges again

--- 2023/Day 5/1.py
+++ 2023/Day 5/1.py
@@ -33,5 +33,46 @@ for i, line in enumerate(allLines):
         destination_start, source_start, range_length = [int(e) for e in partial_map]
         maps[current_map_index].append((source_start, destination_start, range_length))
 
-print(maps)
+# Increment location and mapping backwards 
+# until getting a valid seed, that's the lowest location
+def trace_back_to_seed(location):
+    for map in maps:
+        for partial_map in map:
+            source_start, destination_start, range_length = partial_map
+
+            # print(f"{destination_start} <= L: {location} < {destination_start + range_length} = {(destination_start <= location) and (location < destination_start + range_length)}")
+
+            # Should map from destination to source instead of source to destination
+            if (destination_start <= location) and (location < destination_start + range_length):
+                offset = destination_start - source_start
+                location -= offset
+
+                break
+                # print(f"Transf: {location}")
+    
+    # Now a seed
+    return location
+
 print(seed_ranges)
+
+# Reverse every mapping's order
+maps.reverse()
+for i, map in enumerate(maps):
+    map.reverse()
+    maps[i] = map
+
+print(maps)
+
+location = 0
+found_result = False
+while(not found_result):
+    seed = trace_back_to_seed(location)
+    for i, seed_start in enumerate(seed_ranges):
+        if i % 2 != 0: continue
+
+        seed_end = seed_ranges[i + 1]
+        if (seed_start <= seed) and (seed <= seed_end):
+            found_result = True
+            print(f"Result: {seed}")
+
+    location += 1
