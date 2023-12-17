c797defa5a3587c4379cddf402b81f4893fc2dae
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2023-12-17 16:45:38 -0300

    Day 7 part 1 done, part 2 in progress

--- 2023/Day 7/1.py
+++ 2023/Day 7/1.py
@@ -4,3 +4,79 @@ allLines = []
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 with open(os.path.join(__location__, 'input.txt')) as f:
     allLines = f.readlines()
+
+def get_hand_type(hand):
+    jokers = 0
+    kinds = {}
+    for card in hand:
+        if card == 'J':
+            jokers += 1
+        elif card in kinds:
+            kinds[card] += 1
+        else: kinds[card] = 1
+    
+    values = sorted(kinds.values(), reverse=True)
+    if len(values) == 0: values.append(0)
+    values[0] += jokers
+    kinds_count = len(values)
+
+    if   kinds_count == 1:
+        return 6
+    elif kinds_count == 2:
+        if values[0] == 4:
+            return 5
+        else:
+            return 4
+    elif kinds_count == 3:
+        if values[0] == 3:
+            return 3
+        if values[0] == 2 and values[1] == 2:
+            return 2
+    elif kinds_count == 4:
+        if values[0] == 2:
+            return 1
+    
+    return 0
+
+value_table = "J23456789TJQKA"
+def get_value(hand):
+    value = 0
+    base_13_digit_mult = 1
+
+    hand = hand[::-1]
+    for card in hand:
+        value += base_13_digit_mult * value_table.find(card)
+        base_13_digit_mult *= 13
+
+    return value
+
+most_significant_digit_mult = pow(13, 5)
+values_to_bid = {}
+
+for line in allLines:
+    sline = line.strip()
+
+    hand, bid = sline.split()
+    bid = int(bid)
+
+    # Define type
+    type_value = get_hand_type(hand)
+
+    # Diff inside same type
+    hand_value = get_value(hand)
+
+    value = most_significant_digit_mult * type_value + hand_value
+    values_to_bid[value] = bid
+
+# Sort values to determine rank
+values_to_bid = dict(sorted(values_to_bid.items()))
+print(values_to_bid)
+
+result = 0
+rank = 1
+for value, bid in values_to_bid.items():
+    result += rank * bid
+
+    rank += 1
+
+print(result)
