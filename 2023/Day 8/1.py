df5400a09acf2e73459c8d36b6030ef8c7e01933
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2023-12-17 17:11:06 -0300

    Day 8 innit

---
+++ 2023/Day 8/1.py
@@ -0,0 +1,6 @@
+import os
+
+allLines = []
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+with open(os.path.join(__location__, 'input.txt')) as f:
+    allLines = f.readlines()
