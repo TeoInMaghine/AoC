948db0a6c8868a36adc9d42532c937eb84006974
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2023-12-17 13:58:20 -0300

    Agh day 5 is frustrating me, I'll try again in like a week or something

--- 2023/Day 5/1.py
+++ 2023/Day 5/1.py
@@ -1,5 +1,4 @@
 import os
-import math
 
 allLines = []
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -39,45 +38,86 @@ print(seed_ranges)
 def is_in_range(x, start, length):
     return start <= x and x < start + length
 
-def process():
-    new_seed_ranges = []
+def process(current_map_index):
+    global seed_ranges
+    map = maps[current_map_index]
 
-    for index, seed_start in enumerate(seed_ranges):
-        if index % 2 != 0: continue
+    for partial_map in map:
+        map_start, _, map_length = partial_map
+        new_seed_ranges = []
+        for index, seed_start in enumerate(seed_ranges):
+            if index % 2 != 0: continue
 
-        seed_length = seed_ranges[index + 1]
+            seed_length = seed_ranges[index + 1]
 
-        for map in maps:
-            for partial_map in map:
-                map_start, _, map_length = partial_map
+            map_start_in_range = is_in_range(map_start, seed_start, seed_length)
+            map_end_in_range = is_in_range(map_start + map_length - 1, seed_start, seed_length)
 
-                map_start_in_range = is_in_range(map_start, seed_start, seed_length)
-                map_end_in_range = is_in_range(map_start + map_length - 1, seed_start, seed_length)
+            print(map_start, seed_start, map_length)
+            print(map_start_in_range, map_end_in_range)
 
-                if map_start_in_range and map_end_in_range:
-                    if seed_start != map_start:
-                        new_seed_ranges.append(seed_start)
-                        first_length = map_start - seed_start
-                        new_seed_ranges.append(first_length)
-                    
+            if map_start_in_range and map_end_in_range:
+                if seed_start != map_start:
+                    new_seed_ranges.append(seed_start)
+                    first_length = map_start - seed_start
+                    new_seed_ranges.append(first_length)
+                
+                new_seed_ranges.append(map_start)
+                new_seed_ranges.append(map_length)
+
+                if map_start + map_length != seed_start + seed_length:
+                    last_start = map_start + map_length
+                    last_length = seed_start + seed_length - last_start
+                    new_seed_ranges.append(last_start)
+                    new_seed_ranges.append(last_length)
+            elif map_end_in_range:
+                if map_start + map_length != seed_start + seed_length:
+                    new_seed_ranges.append(seed_start)
+                    first_length = map_start + map_length - seed_start
+                    new_seed_ranges.append(first_length)
+                
+                    last_start = seed_start + first_length
+                    last_length = seed_length - first_length
+                    new_seed_ranges.append(last_start)
+                    new_seed_ranges.append(last_length)
+            elif map_start_in_range:
+                if seed_start != map_start:
+                    new_seed_ranges.append(seed_start)
+                    first_length = map_start - seed_start
+                    new_seed_ranges.append(first_length)
+
+                    last_length = map_length - first_length
                     new_seed_ranges.append(map_start)
-                    new_seed_ranges.append(map_length)
-
-                    if map_start + map_length != seed_start + seed_length:
-                        last_start = map_start + map_length
-                        last_length = seed_start + seed_length - last_start
-                        new_seed_ranges.append(last_start)
-                        new_seed_ranges.append(last_length)
-                elif map_end_in_range:
-                    if map_start + map_length != seed_start + seed_length:
-
-                elif map_start_in_range:
-                    pass
+                    new_seed_ranges.append(last_length)
                 else:
                     new_seed_ranges.append(seed_start)
                     new_seed_ranges.append(seed_length)
+            else:
+                new_seed_ranges.append(seed_start)
+                new_seed_ranges.append(seed_length)
+
+        seed_ranges = new_seed_ranges
+
+    print(seed_ranges)
+    # Convert to soil (at least in the first pass)
+    for index, seed_start in enumerate(seed_ranges):
+        if index % 2 != 0: continue
+
+        for partial_map in map:
+            map_start, map_destintation, map_length = partial_map
+            offset = map_destintation - map_start
+
+            seed_ranges[index] = seed_start + offset
+    print(seed_ranges)
 
 
-    pass
+    current_map_index += 1
+    seed_ranges = new_seed_ranges
 
-process()
+    if current_map_index < len(maps):
+        process(current_map_index)
+    else:
+        print("Result:")
+        print(seed_ranges)
+
+process(0)
