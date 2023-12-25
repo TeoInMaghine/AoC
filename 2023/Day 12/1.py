e473893fe60a0cc3908dbcbb49176cd5e0c18087
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2023-12-25 11:48:51 -0300

    I'mma skip this day for now

--- 2023/Day 12/1.py
+++ 2023/Day 12/1.py
@@ -4,3 +4,56 @@ allLines = []
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 with open(os.path.join(__location__, 'input.txt')) as f:
     allLines = f.readlines()
+
+# Parsing
+rows = []
+for line in allLines:
+    sline = line.strip()
+    conditions, groups = sline.split()
+
+    groups = [int(group) for group in groups.split(",")]
+
+    rows.append((conditions, groups))
+
+def is_valid(conditions, groups):
+    i = 0
+    for group in groups:
+        current_contigous = 0
+        
+        while i < len(conditions):
+            char = conditions[i]
+            if char == '#':
+                current_contigous += 1
+            
+            if current_contigous > group:
+                return False
+            
+            if char == '.' and current_contigous > 0:
+                if current_contigous != group:
+                    return False
+                
+                current_contigous = 0
+                break
+
+            i += 1
+
+            if i >= len(conditions) and current_contigous != group:
+                return False
+    
+    return True
+
+def determine_combinations(conditions, groups):
+    i = 0
+    for group in groups:
+        first_unknown = conditions.find('?')
+
+        if first_unknown == -1:
+            return conditions
+        
+        
+
+for row in rows:
+    conditions, groups = row
+
+
+    print(is_valid(conditions, groups))
