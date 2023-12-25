4ef6756aaa2b00badd039c74c631d225cdfc4f2a
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2023-12-25 14:40:51 -0300

    Day 14 part 2 completed
    I first figured out the cycle start & length, then I used
    those values to only spin the necessary times.
    For that I commented and uncommented code.

--- 2023/Day 14/1.py
+++ 2023/Day 14/1.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 allLines = []
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -6,6 +7,9 @@ with open(os.path.join(__location__, 'input.txt')) as f:
     allLines = f.readlines()
 
 NORTH = (-1, 0)
+WEST = (0, -1)
+SOUTH = (1, 0)
+EAST = (0, 1)
 
 grid = [[char for char in line.strip()] for line in allLines]
 
@@ -33,26 +37,75 @@ def try_moving(pos, dir):
     
     return False
 
-moved = True
-while moved:
-    moved = False
+def tilt_in_dir(dir):
+    moved = True
+    while moved:
+        moved = False
 
+        for r, row in enumerate(grid):
+            for c, char in enumerate(row):
+                if char != 'O': continue
+
+                # Try moving
+                pos = r, c
+                if try_moving(pos, dir):
+                    moved = True
+
+def print_grid():
+    for row in grid:
+        string = ""
+        for char in row:
+            string += char
+        print(string)
+    
+    print()
+
+def spin():
+    tilt_in_dir(NORTH)
+    tilt_in_dir(WEST)
+    tilt_in_dir(SOUTH)
+    tilt_in_dir(EAST)
+
+def get_total_loads():
+    total_loads = 0
     for r, row in enumerate(grid):
-        for c, char in enumerate(row):
+        for char in row:
             if char != 'O': continue
 
-            # Try moving
-            pos = r, c
-            if try_moving(pos, NORTH):
-                moved = True
+            load = ROW_COUNT - r
+            total_loads += load
+    
+    return total_loads
 
+# grids = {}
+# cycle_start = 0
+# cycle_length = 0
+# for i in range(100):
+    # spin()
+    # hashable = str(grid)
+    # if hashable not in grids:
+        # grids[hashable] = i
+    # else:
+        # print("Found a cycle!")
+        # cycle_start = grids[hashable]
+        # cycle_length = i - cycle_start
+        # break
 
-result = 0
-for r, row in enumerate(grid):
-    for c, char in enumerate(row):
-        if char != 'O': continue
+# print(f"Cycle start: {cycle_start}")
+# print(f"Cycle length: {cycle_length}")
 
-        load = ROW_COUNT - r
-        result += load
+cycle_start = 87
+cycle_length = 9
+TARGET = 1_000_000_000
+
+equivalent = (TARGET - cycle_start) % cycle_length + cycle_start
+print(equivalent)
+
+for i in range(equivalent):
+    spin()
+
+result = get_total_loads()
 
 print(result)
+
+# print_grid()
