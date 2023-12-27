a82f4a44ec7fca27366b41e3d10ed15b22dd18e2
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2023-12-27 12:09:51 -0300

    Day 17 part 1 completed
    Damn that was hard, basically what I missed before
    was that my graph had to be 4D instead of just 3D,
    to account for the direction AND consecutive tiles
    going in the same direction.
    This could be optimized more I think, it took like 40 seconds, but it worked so fuck it xd

--- 2023/Day 17/1.py
+++ 2023/Day 17/1.py
@@ -11,13 +11,34 @@ NORTH = 0
 SOUTH = 1
 WEST = 2
 EAST = 3
+opposite_dirs = {NORTH: SOUTH, SOUTH: NORTH, WEST: EAST, EAST: WEST}
+
+# Considers consecutive tiles and directions
+def is_in_bounds(x, y, origin_dir, origin_cons, dest_dir):
+    # This is a very hacky way to get the consecutive tiles in init_base_neighbours working
+    global hacky_cons
+
+    # Don't allow 180º turns
+    if opposite_dirs[origin_dir] == dest_dir:
+        return False
+
+    # Don't allow going in the same direction more than 3 times
+    if origin_dir == dest_dir:
+        if origin_cons >= 2:
+            return False
+        else: hacky_cons = origin_cons + 1
+    else: hacky_cons = 0
+    
+    # Don't allow going out of the grid bounds
+    return y >= 0 and y < ROWS_COUNT and x >= 0 and x < COLS_COUNT
 
 class Node():
-    def __init__(self, heat_loss, pos_y, pos_x, travel_direction):
+    def __init__(self, heat_loss, pos_y, pos_x, travel_direction, consecutive_tiles):
         self.heat_loss = heat_loss
         self.pos_y = pos_y
         self.pos_x = pos_x
         self.travel_direction = travel_direction
+        self.consecutive_tiles = consecutive_tiles
 
         self.parent = None
     
@@ -26,23 +47,22 @@ class Node():
         # G-score and F-score are infinite by default
         self.g_score = math.inf
         self.f_score = math.inf
-
-        # Crucible can only move at most 3 tiles in the same direction
-        # To take that into account, we store the consecutive tiles traveled in the same direction
-        self.consecutive_tiles = 0
     
     # All the nodes need to be generated before doing these, deferred initialization
     def init_base_neighbours(self):
         # Get base neighbours
-        self.neighbours = {}
-        if self.pos_y - 1 >= 0 and self.pos_y - 1 < ROWS_COUNT:
-            self.neighbours[NORTH] = grid[self.pos_y - 1][self.pos_x][NORTH]
-        if self.pos_y + 1 >= 0 and self.pos_y + 1 < ROWS_COUNT:
-            self.neighbours[SOUTH] = grid[self.pos_y + 1][self.pos_x][SOUTH]
-        if self.pos_x - 1 >= 0 and self.pos_x - 1 < COLS_COUNT:
-            self.neighbours[WEST] = grid[self.pos_y][self.pos_x - 1][WEST]
-        if self.pos_x + 1 >= 0 and self.pos_x + 1 < COLS_COUNT:
-            self.neighbours[EAST] = grid[self.pos_y][self.pos_x + 1][EAST]
+        self.neighbours = []
+        if is_in_bounds(self.pos_x, self.pos_y - 1, self.travel_direction, self.consecutive_tiles, NORTH):
+            self.neighbours.append(grid[self.pos_y - 1][self.pos_x][NORTH][hacky_cons])
+
+        if is_in_bounds(self.pos_x, self.pos_y + 1, self.travel_direction, self.consecutive_tiles, SOUTH):
+            self.neighbours.append(grid[self.pos_y + 1][self.pos_x][SOUTH][hacky_cons])
+
+        if is_in_bounds(self.pos_x - 1, self.pos_y, self.travel_direction, self.consecutive_tiles, WEST):
+            self.neighbours.append(grid[self.pos_y][self.pos_x - 1][WEST][hacky_cons])
+
+        if is_in_bounds(self.pos_x + 1, self.pos_y, self.travel_direction, self.consecutive_tiles, EAST):
+            self.neighbours.append(grid[self.pos_y][self.pos_x + 1][EAST][hacky_cons])
     
     # For the priority queue
     def __lt__(self, other):
@@ -52,11 +72,12 @@ class Node():
         return f'({self.pos_y}, {self.pos_x}) {self.heat_loss} F:{self.f_score} G:{self.g_score}'
 
 def reconstruct_path():
+    chars_for_dirs = {NORTH: '^', SOUTH: 'v', WEST: '<', EAST: '>'}
     debug_grid = [['.' for c in row] for row in grid]
     n = current
-    while n != None:
+    while n.parent != None:
         print(n)
-        debug_grid[n.pos_y][n.pos_x] = '#'
+        debug_grid[n.pos_y][n.pos_x] = chars_for_dirs[n.travel_direction]
         n = n.parent
     
     for row in debug_grid:
@@ -69,17 +90,19 @@ def reconstruct_path():
 ROWS_COUNT = len(allLines)
 COLS_COUNT = len(allLines[0]) - 1 # ignore \n
 
-# Parse data
-# Columns and rows like always, but also cardinal directions to have the same node with different travel directions in the open set
-grid = [[[Node(int(char), row, col, i) for i in range(4)] for col, char in enumerate(line.strip())] for row, line in enumerate(allLines)]
+# Parse data:
+# Crucible can only move at most 3 tiles in the same direction,
+# so we make a graph that also includes that
+grid = [[[[Node(int(char), row, col, dir, cons) for cons in range(3)] for dir in range(4)] for col, char in enumerate(line.strip())] for row, line in enumerate(allLines)]
 
 # Initialize
 for row in grid:
     for dirs in row: 
-        for node in dirs:
-            node.init_base_neighbours()
+        for cons in dirs:
+            for node in cons:
+                node.init_base_neighbours()
 
-start_nodes = (grid[0][0][i] for i in range(4))
+start_nodes = (grid[0][0][dir][0] for dir in range(4))
 open_set = []
 for start_node in start_nodes:
     start_node.g_score = 0
@@ -97,25 +120,12 @@ while len(open_set) > 0:
         break
     
     # Find dat path bitch
-    for direction, neighbour in current.neighbours.items():
-        # Discard neighbour when going more than 3 times in the same direction
-        if current.consecutive_tiles > 3 and direction == current.travel_direction:
-            continue
-
+    for neighbour in current.neighbours:
         tentative_g_score = current.g_score + neighbour.heat_loss
         if tentative_g_score < neighbour.g_score:
             neighbour.parent = current
             neighbour.g_score = tentative_g_score
             neighbour.f_score = tentative_g_score + neighbour.h_score
-
-            # neighbour.travel_direction = direction
-            neighbour.consecutive_tiles = 2
-            
-            # Only reset consecutive tiles counter when changed direction
-            if direction == current.travel_direction:
-                neighbour.consecutive_tiles = current.consecutive_tiles + 1
             
             if neighbour not in open_set:
                 heapq.heappush(open_set, neighbour)
-            else:
-                heapq.heapify(open_set)
