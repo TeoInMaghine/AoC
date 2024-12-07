05a2b79b611a4e043068876de2858be7a47cd1a5
TeoWSL <teopiratoguthmann2004@gmail.com>
2024-12-07 18:32:37 +0000

    Day 6 part 2 second try, brute force, not yet
    Brute force seems to work in terms of how much time it takes to get a
    solution, but it seems there is a bug in my code, the answer I gave was
    too high. That means I'm identifying something as a cycle when it isn't.

--- 2024/Day 06/2.cpp
+++ 2024/Day 06/2.cpp
@@ -1,38 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 10;
+const int N = 130;
 
-bool visited[N][N];
+bool visited[4][N][N];
 bool obstacles[N][N];
-// Guard positions
-int x, y;
 
-// Indices of cardinal directions
-const int U_I = 0;
-const int R_I = 2;
-const int D_I = 4;
-const int L_I = 6;
 // Cardinal directions: i1,j1, i2,j2, ...
 int dirs[4*2] = {-1,0, 0,1, 1,0, 0,-1};
 
-int r[N];
-int d[N];
-int l[N];
-int u[N];
 
-int rotate_90(int dir_i) {
+int rotated_90(int dir_i) {
     return (dir_i + 2) % 8;
 }
 
-int main() {
-    // neutral elements -> -inf
-    fill(r, r + N, INT_MIN);
-    fill(d, d + N, INT_MIN);
-    // neutral elements -> +inf
-    fill(l, l + N, INT_MAX);
-    fill(u, u + N, INT_MAX);
+bool try_cicle(int x, int y, int dir_i) {
+    // Reset visited
+    fill_n(&visited[0][0][0], 4 * N * N, false);
 
+    // Add the obstacle if it wouldn't be outside bounds and doesn't exist already
+    int obstacle_y = y + dirs[dir_i];
+    int obstacle_x = x + dirs[dir_i + 1];
+    if (obstacle_x >= N || obstacle_x < 0 || obstacle_y >= N || obstacle_y < 0 || obstacles[obstacle_y][obstacle_x])
+        return false;
+    obstacles[obstacle_y][obstacle_x] = true;
+
+    visited[dir_i/2][y][x] = true;
+
+    // Turn
+    dir_i = rotated_90(dir_i);
+
+    // cout << x << "," << y << " dir: " << dir_i << endl;
+    while (true) {
+        if (visited[dir_i/2][y][x]) {
+            // Restore obstacles before returning
+            obstacles[obstacle_y][obstacle_x] = false;
+            // cout << "Yup" << endl;
+            return true;
+        }
+
+        visited[dir_i/2][y][x] = true;
+
+        int diry = dirs[dir_i];
+        int dirx = dirs[dir_i + 1];
+
+        int newy = y + diry;
+        int newx = x + dirx;
+
+        if (newx >= N || newx < 0 || newy >= N || newy < 0) break;
+
+        if (obstacles[newy][newx]) {
+            // Turn
+            dir_i = rotated_90(dir_i);
+        } else {
+            // Move
+            y = newy;
+            x = newx;
+        }
+    }
+
+    // Restore obstacles before returning
+    obstacles[obstacle_y][obstacle_x] = false;
+    return false;
+}
+
+int main() {
+    // Guard starting position
     int original_y, original_x;
     for (int i = 0; i < N; i++) {
         char c;
@@ -48,94 +81,32 @@ int main() {
         scanf("%c", &c); // Skip \n
     }
 
-    y = original_y;
-    x = original_x;
-    int dir_i = U_I;
-    while (true) {
-
-        int diry = dirs[dir_i];
-        int dirx = dirs[dir_i + 1];
-
-        int newy = y + diry;
-        int newx = x + dirx;
-
-        if (newx >= N || newx < 0 || newy >= N || newy < 0) break;
-
-        // Hay lo que llamo un "endpoint"
-        if (obstacles[newy][newx]) {
-            switch (dir_i) {
-                case R_I:
-                    r[y] = max(r[y], x);
-                    break;
-                case D_I:
-                    d[x] = max(d[x], y);
-                    break;
-                case L_I:
-                    l[y] = min(l[y], x);
-                    break;
-                case U_I:
-                    u[x] = min(u[x], y);
-                    break;
-            }
-
-            dir_i = rotate_90(dir_i);
-        } else { // Move
-            y = newy;
-            x = newx;
-        }
-    }
-
-    int boxes = 0;
-    y = original_y;
-    x = original_x;
-    dir_i = U_I;
-    while (true) {
-
-        cout << x << "," << y << endl;
-        visited[y][x] = true;
-        int eval_dir_i = rotate_90(dir_i);
-        switch (eval_dir_i) {
-            case R_I:
-                boxes += x <= r[y];
-                break;
-            case D_I:
-                boxes += y <= d[x];
-                break;
-            case L_I:
-                boxes += x >= l[y];
-                break;
-            case U_I:
-                boxes += y >= u[x];
-                break;
-        }
-
-
-        int diry = dirs[dir_i];
-        int dirx = dirs[dir_i + 1];
-
-        int newy = y + diry;
-        int newx = x + dirx;
-
-        if (newx >= N || newx < 0 || newy >= N || newy < 0) break;
-
-        // Hay lo que llamo un "endpoint"
-        if (obstacles[newy][newx]) {
-            dir_i = rotate_90(dir_i);
-        } else { // Move
-            y = newy;
-            x = newx;
-        }
-    }
-
     int result = 0;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            result += visited[i][j];
+    // Start upward
+    int dir_i = 0;
+    // Guard current position
+    int x = original_x, y = original_y;
+    while (true) {
+        result += try_cicle(x, y, dir_i);
+
+        int diry = dirs[dir_i];
+        int dirx = dirs[dir_i + 1];
+
+        int newy = y + diry;
+        int newx = x + dirx;
+
+        if (newx >= N || newx < 0 || newy >= N || newy < 0) break;
+
+        if (obstacles[newy][newx]) {
+            // Turn
+            dir_i = rotated_90(dir_i);
+        } else {
+            // Move
+            y = newy;
+            x = newx;
         }
     }
 
     cout << result << endl;
-
-    cout << boxes << endl;
 }
 
