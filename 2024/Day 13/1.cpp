4c9f847da877cc72719cb5f5a7b15a136f30b3ff
TeoWSL <teopiratoguthmann2004@gmail.com>
2024-12-14 00:53:04 +0000

    Day 13 part 1
    IDK, had to look at the subreddit for inspiration on this one. I'm not
    sure I entirely understands why this was the proper way; it turns out
    this gives you the minimum amount of tokens because the system only has
    one solution. It seems there aren't any instances where the vectors
    (ax,ay) and (bx,by) are multiples of each other too.

---
+++ 2024/Day 13/1.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minimum_tokens(int ax, int ay, int bx, int by, int px, int py) {
+
+    int determinant = ax*by - bx*ay;
+    int a_numerator = px*by - bx*py;
+    int b_numerator = ax*py - ay*px;
+    if (a_numerator % determinant == 0 && b_numerator % determinant == 0) {
+        int a = a_numerator / determinant;
+        int b = b_numerator / determinant;
+        return 3*a + b;
+    }
+
+    return 0;
+}
+
+int main() {
+    int answer = 0;
+
+    char c;
+    do {
+        int ax, ay, bx, by, px, py;
+
+        // "Button A: X+"
+        for (int i = 0; i < 12; i++) getchar();
+        scanf("%d", &ax);
+        // ", Y+"
+        for (int i = 0; i < 4; i++) getchar();
+        scanf("%d", &ay);
+
+        // "\nButton B: X+"
+        for (int i = 0; i < 13; i++) getchar();
+        scanf("%d", &bx);
+        // ", Y+"
+        for (int i = 0; i < 4; i++) getchar();
+        scanf("%d", &by);
+
+        // "\nPrize: X="
+        for (int i = 0; i < 10; i++) getchar();
+        scanf("%d", &px);
+        // ", Y="
+        for (int i = 0; i < 4; i++) getchar();
+        scanf("%d", &py);
+        getchar();
+
+        answer += minimum_tokens(ax, ay, bx, by, px, py);
+    } while (scanf("%c", &c) != EOF);
+
+    cout << answer << endl;
+}
