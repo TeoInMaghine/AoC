b1fc7423a08aae68d2480f1c8fe106c12d5f1acb
TeoWSL <teopiratoguthmann2004@gmail.com>
2024-12-20 19:20:08 +0000

    Day 20 part 1

--- 2024/Day 20/1.cpp
+++ 2024/Day 20/1.cpp
@@ -4,15 +4,14 @@ using namespace std;
 struct Node {
     int g;
     bool visited;
-    Node* prev;
+    pair<int,int> prev;
 };
 
-const int N = 15;
-bool is_wall[N][N];
-Node grid[N][N];
+const int N = 141;
+const pair<int,int> dirs[4] = { {-1,0}, {0,1}, {1,0}, {0,-1} };
+const pair<int,int> cheat_offsets[8] = { {-2,0}, {-1,1}, {0,2}, {1,1}, {2,0}, {1,-1}, {0,-2}, {-1,-1} };
 
-pair<int,int> start, finish;
-pair<int,int> dirs[4] = { {-1,0}, {0,1}, {1,0}, {0,-1} };
+Node grid[N][N];
 
 bool comp(const pair<int,int>& a, const pair<int,int>& b) {
     const auto& [ai, aj] = a;
@@ -22,13 +21,18 @@ bool comp(const pair<int,int>& a, const pair<int,int>& b) {
 
 priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(&comp)> unvisited(comp);
 
+pair<int,int> start, finish;
+set<pair<int,int>> path;
+// seconds saved: # of cheats
+map<int,int> cheat_saves;
+
 int main() {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            char c = getchar();
-            switch (c) {
+            bool is_wall = false;
+            switch (getchar()) {
                 case '#':
-                    is_wall[i][j] = true;
+                    is_wall = true;
                     break;
                 case 'S':
                     start = {i,j};
@@ -37,7 +41,7 @@ int main() {
                     finish = {i,j};
                     break;
             }
-            grid[i][j] = { INT_MAX, is_wall[i][j], NULL };
+            grid[i][j] = { INT_MAX, is_wall, {-1,-1} };
         }
         getchar();
     }
@@ -52,7 +56,7 @@ int main() {
 
         if (ci == finish.first && cj == finish.second) break; // found finish
 
-        for (const auto [di,dj] : dirs) {
+        for (const auto& [di,dj] : dirs) {
             const int ni = ci + di, nj = cj + dj;
             Node* next = &grid[ni][nj];
 
@@ -61,11 +65,38 @@ int main() {
             int new_g = curr->g + 1;
             if (new_g < next->g) {
                 next->g = new_g;
-                next->prev = curr;
+                next->prev = {ci,cj};
                 unvisited.push({ni,nj});
             }
         }
     }
 
-    cout << grid[finish.first][finish.second].g << '\n';
+    pair<int,int> curr = finish;
+    do {
+        path.insert(curr);
+        curr = grid[curr.first][curr.second].prev;
+    } while (curr.first != -1 && curr.second != -1);
+
+    for (const auto& [i,j] : path) {
+        const int curr = grid[i][j].g;
+
+        for (const auto& [di,dj] : cheat_offsets) {
+            const int ni = i + di, nj = j + dj;
+
+            if (path.contains({ni,nj})) {
+                const int& next = grid[ni][nj].g;
+                int seconds_saved = next - curr - 2;
+
+                if (seconds_saved > 0)
+                    cheat_saves[seconds_saved]++;
+            }
+        }
+    }
+
+    int answer = 0;
+    for (auto it = cheat_saves.lower_bound(100); it != cheat_saves.end(); ++it) {
+        answer += it->second;
+    }
+
+    cout << answer << '\n';
 }
