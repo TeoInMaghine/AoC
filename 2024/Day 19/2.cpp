081bad247223698a07338d09a1d723f6466c42af
TeoWSL <teopiratoguthmann2004@gmail.com>
2024-12-20 01:39:00 +0000

    Small improvement to Day 19 part 2
    Just replaced map for array in memo, shaved off like 10ms, so not that
    much. One solution that I saw in the subreddit which was cool was using
    a tree, which is something that I actually considered but just could not
    come up with a way to do it. To be fair more code is needed, when really
    this solution is good enough for me.

--- 2024/Day 19/2.cpp
+++ 2024/Day 19/2.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-map<int,long> memo;
+long memo[60];
 set<string> towels;
 string line;
 
 long solve(int start) {
     if (start >= line.size()) return 1;
 
-    if (memo.contains(start)) return memo[start];
+    if (memo[start] != -1) return memo[start];
 
     long solvable = 0;
     for (int count = 1; count < line.size() - start + 1; count++) {
@@ -40,8 +40,8 @@ int main() {
 
     long answer = 0;
     while (getline(cin, line)) {
+        fill_n(&memo[0], 60, -1);
         answer += solve(0);
-        memo.clear();
     }
 
     cout << answer << '\n';
