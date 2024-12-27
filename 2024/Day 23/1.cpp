84fc9c2758a788b8be5dcbab9b3055cb78c9138a
TeoWSL <teopiratoguthmann2004@gmail.com>
2024-12-27 17:10:12 +0000

    Day 23 part 1
    Had to use follow this algo:
    https://www.geeksforgeeks.org/cycles-of-length-n-in-an-undirected-and-connected-graph/
    I probably could've figured it out on my own, but I was just really
    confused on the approach. I'll take note on how this algo works,
    basically DFS but it's intresting how it uses that to count cycles of a
    given length.

--- 2024/Day 23/1.cpp
+++ 2024/Day 23/1.cpp
@@ -1,18 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define PC pair<char,char>
+const int V = 26 * 26;
+const int n = 3;
 
-// PC: PC
-map<PC, set<PC>> connections;
+// PC: connections
+set<int> adj[V];
+bool visited[V] = { false };
+
+void DFS(int n, int v, int start, int& count) {
+    visited[v] = true;
+
+    // path of length (n-1) found
+    if (n == 0) {
+        // unmark to make it usable again
+        visited[v] = false;
+        count += adj[v].contains(start);
+        return;
+    }
+
+    // search every possible path of length (n-1)
+    for (const auto& next : adj[v])
+        if (!visited[next])
+            DFS(n - 1, next, start, count);
+
+    // unmark to make it usable again
+    visited[v] = false;
+}
 
 int main() {
     string s;
     while (getline(cin,s)) {
-        PC pc1, pc2;
-        pc1 = { s[0], s[1] };
-        pc2 = { s[3], s[4] };
-        connections[pc1].insert(pc2);
-        connections[pc2].insert(pc1);
+        int pc1 = 26 * (s[0] - 'a') + s[1] - 'a';
+        int pc2 = 26 * (s[3] - 'a') + s[4] - 'a';
+        adj[pc1].insert(pc2);
+        adj[pc2].insert(pc1);
     }
+
+    int count = 0;
+
+    // check PCs starting with 't'
+    const int from = ('t' - 'a') * 26, end = ('t' - 'a' + 1) * 26;
+    for (int v = from; v < end; v++) {
+        if (adj[v].empty()) continue;
+
+        DFS(n - 1, v, v, count);
+        visited[v] = true;
+    }
+    count /= 2;
+
+    cout << count << '\n';
 }
